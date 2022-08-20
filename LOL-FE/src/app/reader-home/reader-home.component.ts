6c8aad4a773e212b9ce0e57ef05cc34bb1fe6acc
ZQ-SCIS <ziqing.yeo.2020@scis.smu.edu.sg>
2022-08-21 04:37:47 +0800

    LOL-35 Reader Landing Page

--- LOL-FE/src/app/reader-home/reader-home.component.ts
+++ LOL-FE/src/app/reader-home/reader-home.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-reader-home',
   templateUrl: './reader-home.component.html',
-  styleUrls: ['./reader-home.component.css']
+  styleUrls: ['./reader-home.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ReaderHomeComponent implements OnInit {
 
