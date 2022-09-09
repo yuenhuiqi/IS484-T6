155e7cffe6aace5c18c689c267d8c15384d14c06
yuenhuiqi <huiqi.yuen.2019@scis.smu.edu.sg>
2022-09-09 16:12:50 +0800

    [LOL-57] User Authentication – FE
    - UI for user login page
    - yet to implement password authentication & session

--- LOL-FE/src/app/app-routing.module.ts
+++ LOL-FE/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ReaderHomeComponent } from './reader-home/reader-home.component';
 import { UploaderHomeComponent } from './uploader-home/uploader-home.component';
 import { UploadDocumentComponent } from './upload-document/upload-document.component';
+import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
-  { path: '', component: ReaderHomeComponent },
+  { path: '', component: LoginComponent},
+  { path: 'reader', component: ReaderHomeComponent },
   { path: 'uploader', component: UploaderHomeComponent },
-  { path: 'uploader/upload', component: UploadDocumentComponent},
+  { path: 'uploader/upload', component: UploadDocumentComponent}
 ]
 
 @NgModule({
