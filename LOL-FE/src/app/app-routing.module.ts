3560c3d1d756638f33228f1ad0560c0789ac6447
krystenng <79707828+krystenng@users.noreply.github.com>
2022-09-16 23:21:37 +0800

    LOL-58 - Edit document details
    Added the function to preview document in a new tab.

--- LOL-FE/src/app/app-routing.module.ts
+++ LOL-FE/src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { ReaderHomeComponent } from './components/reader-home/reader-home.compon
 import { UploaderHomeComponent } from './components/uploader-home/uploader-home.component';
 import { UploadDocumentComponent } from './components/upload-document/upload-document.component';
 import { LoginComponent } from './components/login/login.component';
-import { EditDocumentDetailsComponent } from './components/edit-document-details/edit-document-details.component';
 import { UploadSuccessComponent } from './components/upload-success/upload-success.component';
 import { ViewDocumentComponent } from './components/docviewer/doc-viewer.component';
 
@@ -14,7 +13,6 @@ const routes: Routes = [
   { path: 'reader', component: ReaderHomeComponent },
   { path: 'uploader', component: UploaderHomeComponent },
   { path: 'uploader/upload', component: UploadDocumentComponent},
-  { path: 'uploader/upload/editDetails', component: EditDocumentDetailsComponent},
   { path: 'uploader/upload/success', component: UploadSuccessComponent },
   { path: 'viewdocument', component: ViewDocumentComponent}
 ]
