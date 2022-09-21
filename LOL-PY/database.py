f1d8e3309ffb6e3149da47defc82fa8220b80fa2
night-skhye <edena.teo.2019@sis.smu.edu.sg>
2022-09-21 16:27:07 +0800

    Update database.py

--- LOL-PY/database.py
+++ LOL-PY/database.py
@@ -2,7 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import *
 
-DB_URI = 'mysql+mysqlconnector://root:root@localhost:3306/fypdb'
+# DB_URI = 'mysql+mysqlconnector://root:root@localhost:3306/fypdb'
+DB_URI = 'mysql+mysqlconnector://admin:lesgoAdmin@database-fyp.c8ob3ug8qjhh.ap-southeast-1.rds.amazonaws.com:3306/fypdb'
+
 app = Flask(__name__)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
 app.config["SQLALCHEMY_DATABASE_URI"]= DB_URI
