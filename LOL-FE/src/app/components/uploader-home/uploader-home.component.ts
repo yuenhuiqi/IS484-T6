d5ba512e8a4495149e444d962f2333ddb016ff4c
krystenng <79707828+krystenng@users.noreply.github.com>
2022-10-02 23:38:02 +0800

    fixes for reader and uploader UI
    change naming for reader home and fixes for snackbar timing

--- LOL-FE/src/app/components/uploader-home/uploader-home.component.ts
+++ LOL-FE/src/app/components/uploader-home/uploader-home.component.ts
@@ -43,7 +43,7 @@ export class UploaderHomeComponent implements OnInit {
   }
 
   viewDocument(docID: any): void {
-    location.assign(`/viewdocument/${docID}`)
+    window.open(`/viewdocument/${docID}`)
   }
 
   deleteDoc(docName: any) {
