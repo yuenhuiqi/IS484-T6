616f0a5107b49240b29f957478f30c6f8a8ccb9d
yuenhuiqi <huiqi.yuen.2019@scis.smu.edu.sg>
2022-10-05 15:35:59 +0800

    [LOL-79] AI/FE Integration for search results

--- LOL-FE/src/app/components/view-results-process/view-results-process.component.ts
+++ LOL-FE/src/app/components/view-results-process/view-results-process.component.ts
@@ -11,18 +11,42 @@ export class ViewResultsProcessComponent implements OnInit {
   panelOpenState = false;
   sub: any;
   query: any;
+  docArr: any;
+  docDict: any = {};
 
   constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit(): void {
+
+    if (localStorage.getItem('reload') == null || localStorage.getItem('reload') == '0') {
+      localStorage.setItem('reload', '1')
+      location.reload()
+    } else {
+      if (localStorage.getItem('reload') == '1') {
+        localStorage.setItem('reload', '0')
+      }
+    }
+
     this.sub = this.route.params.subscribe(params => {
       this.query = decodeURIComponent(params['query']);
     });
 
     this.http.post<any>(`http://18.142.140.202/search`, {"query": this.query})
     .subscribe(
-      data => { console.log(data.documents) }
+      data => { 
+        // console.log(data)
+        for (let i in data.documents) {
+          // console.log(data.documents)
+          if (Object.keys(this.docDict).includes(data.documents[i].meta.doc_uuid)) {
+            this.docDict[data.documents[i].meta.doc_uuid].push([data.documents[i].meta.page, data.documents[i].content])
+          } else {
+            this.docDict[data.documents[i].meta.doc_uuid] = [[data.documents[i].meta.page, data.documents[i].content]]
+          }
+          }
+        }
     )
+
+    
   }
 
 }
