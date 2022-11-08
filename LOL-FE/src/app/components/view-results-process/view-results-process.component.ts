0667b422859be4d2dd116c0ee380591e9b23e594
night-skhye <72493061+night-skhye@users.noreply.github.com>
2022-11-08 22:30:20 +0800

    fix conflicts

--- LOL-FE/src/app/components/view-results-process/view-results-process.component.ts
+++ LOL-FE/src/app/components/view-results-process/view-results-process.component.ts
@@ -31,12 +31,43 @@ export class ViewResultsProcessComponent implements OnInit {
       this.query = decodeURIComponent(params['query']);
     });
 
+<<<<<<< Updated upstream
     this.http.post<any>(`http://18.142.140.202/search`, {"query": this.query})
+=======
+    this.getAcronym()
+
+    console.log(this.encodedQuery)
+    this.http.get<any>(`http://localhost:2222/getSuggestedQueries/` + this.encodedQuery).subscribe(
+      data => {this.relevantSearches = data.suggestedSearches}
+    )
+
+
+
+    this.http.post<any>(`https://18.142.140.202/search`, {"query": this.query})
+>>>>>>> Stashed changes
     .subscribe(
       data => { 
         // console.log(data)
         for (let i in data.documents) {
+<<<<<<< Updated upstream
           // console.log(data.documents)
+=======
+          console.log(data.documents)
+          this.manageDocs.getDocDetails(data.documents[i].meta.doc_uuid)
+          .subscribe(res => { 
+            // this.name = (<any>res).docTitle
+            // this.docNameList.push((<any>res).docTitle)
+            // console.log(docName)
+            //calculate
+            searchID, docID, score
+            score = data.documents[i].meta.score
+
+            this.docDict[data.documents[i].meta.doc_uuid][i].push((<any>res).docTitle)
+            // console.log(this.docNameList)
+            // console.log(this.docDict)
+          }, err => console.log(err));
+
+>>>>>>> Stashed changes
           if (Object.keys(this.docDict).includes(data.documents[i].meta.doc_uuid)) {
             this.docDict[data.documents[i].meta.doc_uuid].push([data.documents[i].meta.page, data.documents[i].content])
           } else {
@@ -46,6 +77,53 @@ export class ViewResultsProcessComponent implements OnInit {
         }
     )
 
+<<<<<<< Updated upstream
+=======
+
+    this.http.get<any>(`http://localhost:2222/getSuggestedQueries/` + this.query+'/'+).subscribe(
+      data => {this.relevantSearches = data.suggestedSearches}
+    )
+    
+    this.getSuggestedQuery("")
+
+    this.newquery.valueChanges.subscribe(val => {
+      this.getSuggestedQuery(val)
+    })
+
+  }
+
+  viewDocument(docID: any): void {
+    this.managefeedback.addFeedbackCount(this.query, docID)
+    .subscribe(res => {
+      console.log(res)
+    });
+    window.open(`/uploader/viewdocument/${docID}/${this.query}`)
+  }
+
+  getSuggestedQuery(qn:string) {
+    qn = encodeURIComponent(qn)
+    this.manageSearchQueryService.getSearchQuery(qn)
+    .subscribe(res => {
+      console.log(res)
+      this.filtered = res
+      this.suggestedQueries.next(this.filtered.data.queryList);
+    });
+  }
+
+  getAcronym() {
+    console.log(this.encodedQuery)
+    this.http.get<any>(`http://localhost:2222/getAllAcronyms/` + this.encodedQuery)
+    .subscribe(
+      data => {
+        for (let i in data.acronyms) {
+          this.found_acronyms.push({
+            'acronym': data.acronyms[i].acronym,
+            'meaning': data.acronyms[i].meaning
+          })
+        }
+        console.log(this.found_acronyms)
+      })
+>>>>>>> Stashed changes
     
   }
 
---
+++ LOL-PY/feedback.py
@@ -0,0 +1,107 @@
+from database import *
+from flask import jsonify
+
+
+class Feedback(db.Model):
+    __tablename__ = 'feedback'
+
+    fID = db.Column(db.Integer, primary_key=True)
+    fSearchID = db.Column(db.String(200), nullable=False)
+    fDocID = db.Column(db.String(200), nullable=False)
+    count = db.Column(db.Integer, nullable=False)  # no of user clicks on document
+    merit = db.Column(db.Integer, nullable=False)  # feedback
+    demerit = db.Column(db.Integer, nullable=False)
+
+    def init(self, fSearchID, fDocID, count):
+        self.fSearchID = fSearchID
+        self.fDocID = fDocID
+        self.count = count 
+        self.merit = 0
+        self.demerit = 0
+
+    def json(self):
+        return {
+            "fID": self.fID,
+            "fSearchID": self.fSearchID,
+            "fDocID": self.fDocID,
+            "count": self.count,
+            "merit": self.merit,
+            "demerit": self.demerit
+        }
+
+
+def get_feedback(searchID, docID):
+    search_ID = '{0}'.format(searchID)
+    doc_ID = '{0}'.format(docID)
+
+    try:
+        feedback = Feedback.query.filter_by(fSearchID=search_ID, fDocID=doc_ID).first()
+        print(search_ID, doc_ID, "feedback exist")
+        return 200, feedback.json()
+    except Exception as e:
+        return 400, ("none found")
+
+
+
+def add_querydoc_count(searchID, docID):
+    search_ID = '{0}'.format(searchID)
+    doc_ID = '{0}'.format(docID)
+
+    if db.session.query(exists().where(Feedback.fSearchID == search_ID)).scalar() and db.session.query(exists().where(Feedback.fDocID == doc_ID)).scalar():
+        print(search_ID, doc_ID, "feedback exist")
+        currentFeedback = Feedback.query.filter_by(fSearchID=search_ID, fDocID=doc_ID).first()
+        currentFeedback.count += 1
+        try:
+            db.session.commit()
+            return 200, "feedback count updated"
+        except Exception as e:
+            print("Something Happened (Update DocSearch Query Count): ", e)
+            return 400, e
+        
+    else:
+        print(search_ID, doc_ID, "new feedback")
+        newFeedback = Feedback(fSearchID = search_ID, fDocID = doc_ID, count=1, merit = 0, demerit = 0)
+        try: 
+            db.session.add(newFeedback)
+            db.session.commit()
+            return 200, "Feedback added to DB!"
+
+        except Exception as e:
+            print("Something Happened (Add new DocSearch Query): ", e)
+            return 400, e
+        
+
+def update_feedback(searchID, docID, score):
+    # print(searchID, docID, score)
+
+    score = int(score)
+
+    try:
+        currentFeedback = Feedback.query.filter_by(fSearchID=searchID, fDocID=docID).first()
+
+        print(currentFeedback.fDocID)
+        if (score > 0):
+            currentFeedback.merit += 1
+            print(currentFeedback.merit, "after commit")
+
+        else:
+            currentFeedback.demerit += 1
+            print(currentFeedback.demerit, "after commit")
+            
+
+        db.session.commit()
+
+        return 200, "Feedback updated"
+
+    except Exception as e:
+        print("Something Happened (Update Feedback Count): ", e)
+        return 400, e
+
+
+
+
+
+
+
+
+
