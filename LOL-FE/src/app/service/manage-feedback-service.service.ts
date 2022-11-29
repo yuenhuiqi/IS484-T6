4e3d9bf42e2ee29062caa3deb4ec7f8ef2b3bd12
Chuen Kai <chuenkaiong@gmail.com>
2022-11-29 16:01:36 +0800

    change to call localhost

--- LOL-FE/src/app/service/manage-feedback-service.service.ts
+++ LOL-FE/src/app/service/manage-feedback-service.service.ts
@@ -6,7 +6,8 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ManageFeedbackServiceService {
 
-  private baseurl = "https://54.254.54.186:2222/"
+  // private baseurl = "https://54.254.54.186:2222/"
+  private baseurl = "https://localhost:2222/"
 
   constructor(private http: HttpClient) { }
 
